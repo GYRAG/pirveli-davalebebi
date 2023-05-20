66f17b025a3951ae188cde9440dbf5f454b3d4cc
Gary Kinkladze <110338310+GYRAG@users.noreply.github.com>
2023-05-20 22:30:24 +0400

    Add files via upload

---
+++ 4davaleba.py
@@ -0,0 +1,5 @@
+number1 = float(input("პირველი რიცხვი: "))
+number2 = float(input("მეორე რიცხვი: "))
+
+result = "პირველი რიცხვი მეტია." if number1> number2 else "მეორე რიცხვი მეტია." if number1 < number2 else "რიცხვები ტოლია ტოლია."
+print(result)
---
+++ 8davaleba.py
@@ -0,0 +1,7 @@
+import math
+
+num1 = int(input("პირველი რიცხვი: "))
+num2 = int(input("მეორე რიცხვი: "))
+
+result = math.gcd(num1, num2)
+print("უდიდესი საერთო გამყოფია:", result)
